fea3f22de82af93524b5486bd8a93e1637db4e08
Zain <zainarif00456@gmail.com>
2022-12-04 16:30:10 +0500

    Starting Work Client Side

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>
+        Animus Chat Application
+      </h1>
+    </>
+  );
+}
+
+export default App;
--- server/main.js
+++ server/main.js
@@ -4,8 +4,28 @@ const http = require('http')
 const cors = require('cors')
 app.use(cors())
 const server = http.createServer(app);
-
+const {Server} = require('socket.io')
 const PORT = 5000;
+
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://0.0.0.0:5000',
+        methods: ['GET', 'POST']
+
+    }, 
+});
+
+// Calling Socket IO.
+io.on('connection', (socket)=> {
+    console.log("USER ID: " + socket.id);
+    
+    socket.on('disconnect', ()=> {
+        console.log("USER Disconnected: " + socket.id);
+    })
+}
+
+
 
 server.listen(PORT, ()=> {
     console.log(`Server listening on port ${PORT}`);
