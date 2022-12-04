2ba732182d274ed3aaf727c84043bf7ff94a8acf
Zain <zainarif00456@gmail.com>
2022-12-04 15:53:22 +0500

    Missing Server Code.

--- server/main.js
+++ server/main.js
@@ -1,1 +1,12 @@
 const express = require('express')
+const app = express()
+const http = require('http')
+const cors = require('cors')
+app.use(cors())
+const server = http.createServer(app);
+
+const PORT = 5000;
+
+server.listen(PORT, ()=> {
+    console.log(`Server listening on port ${PORT}`);
+});
